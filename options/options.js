31b1580e5e37966ec9572efeafd46e597418eb22
Crowdbotics <team@crowdbotics.com>
2021-10-22 19:15:37 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "purple_shadow_31657",
+  url: "https://purple_shadow_31657.botics.co",
+  api: "https://purple_shadow_31657.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
